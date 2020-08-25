5ba3d848f17534a95a5b96c8b30eb594516b7597
JacquesWelgemoed <jacqueswelgemoed@gmail.com>
2020-08-25 16:27:25 +0200

    added frameType global variable, fixed syntax errors

--- libraries/AP_Motors/AP_MotorsMatrix.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix.cpp
@@ -17,6 +17,7 @@
 #include "AP_MotorsMatrix.h"
 
 extern const AP_HAL::HAL& hal;
+AP_Motors::motor_frame_type frameType;
 
 // init
 void AP_MotorsMatrix::init(motor_frame_class frame_class, motor_frame_type frame_type)
@@ -24,7 +25,7 @@ void AP_MotorsMatrix::init(motor_frame_class frame_class, motor_frame_type frame
     // record requested frame class and type
     _last_frame_class = frame_class;
     _last_frame_type = frame_type;
-
+    frameType = frame_type;
     // setup the motors
     setup_motors(frame_class, frame_type);
 
@@ -314,7 +315,7 @@ void AP_MotorsMatrix::output_armed_stabilizing()
     // add scaled roll, pitch, constrained yaw and throttle for each motor
     for (i = 0; i < AP_MOTORS_MAX_NUM_MOTORS; i++) {
         if (motor_enabled[i]) {
-            if (motor_frame_type == MOTOR_FRAME_TYPE_MAO_VTOL)
+            if (frameType == MOTOR_FRAME_TYPE_MAO_VTOL)
             {
                 if(i>1)
                 {
@@ -631,10 +632,11 @@ void AP_MotorsMatrix::setup_motors(motor_frame_class frame_class, motor_frame_ty
                 case MOTOR_FRAME_TYPE_MAO_VTOL:
                     add_motor_raw(AP_MOTORS_MOT_1, 0.0f,0.0f,0.0f,1);
                     add_motor_raw(AP_MOTORS_MOT_2, 0.0f,0.0f,0.0f,4);
-                    add_motor(AP_MOTORS_MOT_4,   45, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 2);
                     add_motor(AP_MOTORS_MOT_3, -135, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 5);
+                    add_motor(AP_MOTORS_MOT_4,   45, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 2);
                     add_motor(AP_MOTORS_MOT_5,  -45, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  6);
                     add_motor(AP_MOTORS_MOT_6,  135, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
+                    break;
                 case MOTOR_FRAME_TYPE_X:
                     add_motor(AP_MOTORS_MOT_1,  90, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  2);
                     add_motor(AP_MOTORS_MOT_2, -90, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 5);
